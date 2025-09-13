41dc12df46be0970935e89dfdf2492ccd19953f9
pv.dev <pedro.v.r.brito@gmail.com>
2025-09-12 23:42:15 -0300

    Reforma da logica do sistema
    Reforma da logica do sistema e simplificação

---
+++ base_dados/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+#define BOOL int
+#define TRUE 1
+#define FALSE 0
+#define NOME 50
+
+struct aluno{
+    char matricula;
+    char nome[NOME];
+    char turma[NOME];
+    char senha[30];
+};
+
+struct professor{
+    char nome[NOME];
+    char email[30];
+    char disciplina[30];
+    char senha[30];
+};
+
+struct turma{
+    char nome[NOME];
+    int quantidade_alunos;
+};
+
+struct atividade{
+    char titulo[NOME];
+    char descricao[150];
+    char nome_turma[NOME];
+    BOOL status;
+};
+
+
+int main(){
+
+}
+
+void incluir_aluno(){
+
+}
+
+void incluir_professor(){
+
+}
+
+void criar_turma(){
+
+}
+
+void criar_atividade(){
+
+}
