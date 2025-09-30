0fc6531d3c83086846ef365ce5ad837ab11574ce
pv.dev <pedro.v.r.brito@gmail.com>
2025-09-30 15:40:48 -0300

    REFINAMENTO
    refinamento de código

--- base_dados/test.c
+++ base_dados/test.c
@@ -22,6 +22,7 @@ struct professor{
 
 struct turma{
     char nome[NOME];
+<<<<<<< Updated upstream
     int quantidade_alunos;
 };
 
@@ -32,10 +33,27 @@ struct atividade{
     BOOL status;
     time_t data_expiracao;
 };
+=======
+    char turma[NOME];
+    char senha[20];
+    Disciplina disciplinas[10];
+}Aluno;
+
+int qtd;
+
+// struct atividade{
+//     char titulo[NOME];
+//     char descricao[150];
+//     char turma[NOME];
+//     char status[20];
+//     time_t data_expiracao;
+// };
+>>>>>>> Stashed changes
 
 
 int main(){
 
+<<<<<<< Updated upstream
 }
 
 void incluir_aluno(){
@@ -52,7 +70,79 @@ void criar_turma(){
 
 // void criar_atividade(){
 
-// }
+=======
+    int escolha;
 
-//ronaldo
+     while (escolha != 7){
+        menu();
+        scanf("%d", &escolha);
+
+        switch (escolha)
+        {
+        //cadastrar novo aluno
+        case 1:
+            
+            printf("Quantos alunos deseja incluir?");
+            scanf("%i", &qtd);
+            // loop for cadastrando os alunos
+            
+            break;
+
+        //deletar aluno
+        case 2:
+            // aaaa
+            break;
+
+        //listar alunos
+        case 3:
+            // listar todos os alunos de uma turma
+            break;
+        case 4:
+            printf("Quantos professores deseja incluir?");
+            scanf("%i", &qtd);
+            // for loop incluindo cadastro dos professores
+            break;
+        case 5:
+            // aaaa
+            break;
+        case 6:
+            // aaaa
+            break;
+        
+        case 7:
+            printf("Saindo...");
+            break;
+        default:
+            printf("Opção inválida! Tente novamente.\n");
+        }
+    }
+}
+
+void menu(){
+    int opt;
+    printf("\n=== SISTEMA DE GESTÃO ===\n");
+    printf("1. Cadastrar novo aluno\n");
+    //printf("2
+    printf("3. Listar todos os alunos da turma\n");
+    printf("4. Cadastrar novo professor\n");
+    printf("5. Listar todos os professor\n");
+    //printf("6. Deletar professor\n");
+    printf("7. Sair\n");
+    printf("Escolha uma opção: ");
+    scanf("%i\n", &opt);
+}
+
+
+//PROTOTIPO
+// void incluir_aluno(char matricula, char nome, char turma){
+//     Aluno aluno;
+
+//     aluno.disciplinas;
+//     aluno.matricula;
+//     aluno.nome;
+//     aluno.turma;
+
+
+>>>>>>> Stashed changes
+// }
 
